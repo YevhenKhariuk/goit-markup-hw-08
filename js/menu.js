91ac233240f9294e5cbdd0977e7831dea4a5d353
YevhenKhariuk <777genya2009@gmail.com>
2022-09-08 01:44:29 +0300

    DZ PATCH 2.0

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,7 @@
     openMenuBtn: document.querySelector(".mobile-menu"),
     closeMenuBtn: document.querySelector(".mobile-menu-closed"),
     menu: document.querySelector(".header-mob-menu"),
+    body: document.querySelector("body"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
@@ -10,5 +11,6 @@
 
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
   }
 })();
